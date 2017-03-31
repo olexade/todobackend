27763f3496cfb039dcef57465fb8bcc3cc04193e
root <root@openstack.local>
2017-03-31 15:29:19 +0300

    update src

---
+++ src/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup (
+  name                 = "todobackend",
+  version              = "0.1.0",
+  description          = "Todobackend Django REST service",
+  packages             = find_packages(),
+  include_package_data = True,
+  scripts              = ["manage.py"],
+  install_requires     = ["Django>=1.10.6,<2.0",
+                          "django-cors-headers>=2.0.2",
+                          "djangorestframework>=3.5.4",
+                          "MySQL-python>=1.2.5",
+						  "uwsgi>=2.0"],
+  extras_require       = {
+                            "test": ["colorama>=0.3.7",
+                              "coverage>=4.3.4",
+                              "django-nose>=1.4.4",
+                              "nose>=1.3.7",
+                              "pinocchio>=0.4.2"
+                            ]
+                         }
+)
